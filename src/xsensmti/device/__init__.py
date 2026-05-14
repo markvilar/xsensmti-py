02fa436d58257d351ee3c82bf3b5fc70cb919c15
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2026-05-14 12:46:31 +0200

    Add XbusStreamReader. Remove reading functions from MtiDevice and add XbusStreamReader member.

--- src/xsensmti/device/__init__.py
+++ src/xsensmti/device/__init__.py
@@ -5,5 +5,7 @@ Device handle and state types for XSens MTi devices.
 from .datatypes import MtiDeviceState as MtiDeviceState
 from .device import MtiDevice as MtiDevice
 from .device import OutputConfig as OutputConfig
+from .xbus_reader import XbusStreamReaderState as XbusStreamReaderState
+from .xbus_reader import XbusStreamReader as XbusStreamReader
 
 __all__: list[str] = []
